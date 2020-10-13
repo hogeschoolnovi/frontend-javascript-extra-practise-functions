38617f9e2b78c39b950d25221d56f00c35175ecd
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-10-13 11:31:27 +0200

    Made practise exercises

---
+++ README.md
@@ -0,0 +1,3 @@
+# Opdrachtbeschrijving
+
+Maak de opdrachten in functions.js en log de uitkomsten in de console door `nodemon functions.js` te runnen. De uitwerkingen staan op de branch _Uitwerkingen_.
---
+++ functions.js
@@ -0,0 +1,90 @@
+/* Opdracht 1 */
+// Schrijf een functie die een naam verwacht en een groet teruggeeft
+// ---- Verwachte uitkomsten:
+// "Nova" geeft "Hoi Nova!"
+// "Nick" geeft "Hoi Nick!"
+
+
+
+
+/* Opdracht 2 */
+// Schrijf een functie die twee parameters verwacht: een woord en een karakter
+// De functie omwikkelt het woord met het karakter en geeft dit terug
+// ---- Verwachte uitkomsten:
+// "bril", "*" geeft "*bril*"
+// "beep", "_" geeft "_beep_"
+// "kaas", "Q" geeft "QkaasQ"
+
+
+
+
+/* Opdracht 3 */
+// Schrijf een functie die een array van strings verwacht. De functie geeft
+// alle strings aan elkaar geplakt terug. Je mag hier geen array- of string methoden
+// voor gebruiken zoals .concat()
+// ---- Verwachte uitkomsten:
+// ["abra", "cadabra"] geeft "abracadabra"
+// ["a", "b", "c"] geeft "abc"
+
+
+
+
+
+/* Opdracht 4 */
+// Schrijf een functie genaamd lastEntry die de laatste entry van een array teruggeeft.
+// Als de parameter n wordt meegegeven, worden de laatste n entries van de array teruggegeven.
+// ---- Verwachte uitkomsten:
+// lastEntry([3, 6, 9, 17, 4, 6, 25, 4]) geeft 4
+// lastEntry([46, 65, 34, 204, 190, 89], 3) geeft [204, 190, 89]
+
+
+
+/* Opdracht 5 */
+// Schrijf een functie die een array van getallen verwacht. De functie geeft het hoogste
+// getal in de array terug. Je mag hier geen array-object methoden voor gebruiken zoals .max()
+// ---- Verwachte uitkomsten:
+// [3, 6, 9, 17, 4, 6, 25] geeft 25
+// [46, 65, 34, 204, 190, 89] geeft 204
+
+
+
+
+
+
+/* Opdracht */
+// Schrijf een functie die geen parameters verwacht en de getallen 1 tot 100 print.
+// Voor getallen die deelbaar zijn door 3 print je "Fizz" in plaats van het getal.
+// Voor getallen die deelbaar zijn door 5 print je "Buzz" in plaats van het getal.
+// Voor getallen die zowel deelbaar zijn door 3 als door 5, print je "FizzBuzz".
+// ---- Verwachte uitkomst:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+// 16
+// 17
+// Fizz
+// 19
+// Buzz
+// Fizz
+// 22
+// 23
+// Fizz
+// Buzz
+// 26
+// Fizz
+// 28
+// 29
+// FizzBuzz
+// etc.
