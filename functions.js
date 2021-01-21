bd208e04701391b7d12d6003a489856ed4cdcb25
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2021-01-21 16:57:05 +0100

    Added exercises

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+// -------------------------------  BEGINNER
+
 /* Opdracht 1 */
 // Schrijf een functie die een naam verwacht en een groet teruggeeft
 // ---- Verwachte uitkomsten:
@@ -6,31 +8,81 @@
 
 
 
-
 /* Opdracht 2 */
-// Schrijf een functie die twee parameters verwacht: een woord en een karakter
-// De functie omwikkelt het woord met het karakter en geeft dit terug
+// Schrijf een functie die een hoeveelheid minuten verwacht (als een getal) en teruggeeft
+// hoeveel seconden dat zijn.
 // ---- Verwachte uitkomsten:
-// "bril", "*" geeft "*bril*"
-// "beep", "_" geeft "_beep_"
-// "kaas", "Q" geeft "QkaasQ"
-
+// 1 geeft 60
+// 3 geeft 180
+// 23 geeft 1840
 
 
 
 /* Opdracht 3 */
-// Schrijf een functie die een array van strings verwacht. De functie geeft
-// alle strings aan elkaar geplakt terug. Je mag hier geen array- of string methoden
-// voor gebruiken zoals .concat()
+// Schrijf een functie die een cijfer verwacht en teruggeeft of het cijfer groter is
+// dan nul
 // ---- Verwachte uitkomsten:
-// ["abra", "cadabra"] geeft "abracadabra"
-// ["a", "b", "c"] geeft "abc"
-
-
+// -3 geeft false
+// 0 geeft false
+// 300 geeft true
 
 
 
 /* Opdracht 4 */
+// Schrijf een functie die twee getallen verwacht en teruggeeft of ze, opgetelt, gróter zijn dan 100.
+// ---- Verwachte uitkomsten:
+// 1 en 23 geeft false
+// 8 en 92 geeft false
+// 89 en 14 geeft true
+
+
+
+/* Opdracht 5 */
+// Schrijf een functie die een array van strings verwacht. Hoe lang die array is, weet je niet van tevoren,
+// dus het moet werken voor alle lengtes. Zelfs voor een lengte van 100 (dus niet één voor één uitschrijven!)
+// De functie geeft alle strings aan elkaar geplakt terug. Je mag hier geen array- of string methoden
+// voor gebruiken zoals .concat()
+// ---- Verwachte uitkomsten:
+// ["abra", "cadabra"] geeft "abracadabra"
+// ["a", "b", "c", "d", "e"] geeft "abcde"
+
+
+
+// -------------------------------  INTERMEDIATE
+
+/* Opdracht 6 */
+// Schrijf een functie die een woord verwacht en dit omgedraait teruggeeft. Je kunt dit zowel handmatig
+// als met ingebouwde string- en array methoden doen. Probeer beide manieren uit te werken als je
+// graag wil oefenen!
+// ---- Verwachte uitkomsten:
+// "koekje" geeft "ejkeok"
+// "vrienden" geeft "nedneirv"
+
+
+
+/* Opdracht 7 */
+// Schrijf een functie die een woord verwacht checkt of dit woord een palindroom is. Een palindroom is een
+// spiegelwoord: het is hetzelfde zowel vooruit als achterstevoren. Als dit zo is, geeft de functie true terug,
+// zo niet, dan false.
+// ---- Verwachte uitkomsten:
+// "lepel" geeft true
+// "madam" geeft true
+// "vrienden" geeft false
+
+
+
+/* Opdracht 8 */
+// Schrijf een functie die een string en een letter verwacht. De functie telt hoe vaak die letter voorkomt in
+// de string en geeft dit terug. Je mag hiervoor geen string- of array-methoden gebruiken.
+// ---- Verwachte uitkomsten:
+// "Hans en marietje lopen naar de supermarkt" en "e" geeft 5
+// "Hans is zijn mondkapje vergeten" en "a" geeft 2
+
+
+
+// ------------------------------- ADVANCED
+
+/* Opdracht 9 */
 // Schrijf een functie genaamd lastEntry die de laatste entry van een array teruggeeft.
 // Als de parameter n wordt meegegeven, worden de laatste n entries van de array teruggegeven.
 // ---- Verwachte uitkomsten:
@@ -39,7 +91,7 @@
 
 
 
-/* Opdracht 5 */
+/* Opdracht 10 */
 // Schrijf een functie die een array van getallen verwacht. De functie geeft het hoogste
 // getal in de array terug. Je mag hier geen array-object methoden voor gebruiken zoals .max()
 // ---- Verwachte uitkomsten:
@@ -48,10 +100,7 @@
 
 
 
-
-
-
-/* Opdracht */
+/* Opdracht 11 */
 // Schrijf een functie die geen parameters verwacht en de getallen 1 tot 100 print.
 // Voor getallen die deelbaar zijn door 3 print je "Fizz" in plaats van het getal.
 // Voor getallen die deelbaar zijn door 5 print je "Buzz" in plaats van het getal.
